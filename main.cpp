2354dd9aafc810bbcc7b80a862bfb8083137609a
matkatmusic <matkatmusic@gmail.com>
2020-04-27 22:24:40 +0000

    converted pointers to references

--- main.cpp
+++ main.cpp
@@ -50,12 +50,12 @@ struct U
 {
     float nameOne { 0.f }, nameTwo { 0.f };
     
-    float staticFunctionReplicate(float* updatedValue )        //12
+    float staticFunctionReplicate(float& updatedValue )        //12
     {
-        if (updatedValue != nullptr)
-        {
+        // if (updatedValue != nullptr)
+        // {
             std::cout << "U's nameOneRepl value: " << this->nameOne << std::endl;
-            this->nameOne = *updatedValue;
+            this->nameOne = updatedValue;
             
             std::cout << "U's nameOneRepl updated value: " << this->nameOne << std::endl;
             
@@ -73,50 +73,38 @@ struct U
             std::cout << "U's nameTwoRepl updated value: " << this->nameTwo << std::endl;
             
             return this->nameTwo * this->nameOne;
-        }
+        // }
         
-        std::cout << "updated val is nullptr!" << std::endl;
-        return 0;
+        // std::cout << "updated val is nullptr!" << std::endl;
+        // return 0;
     }
 };
 
 struct StructNameTwo
 {
-    static float staticFunctionA(U* that, float* updatedValue )        //10
+    static float staticFunctionA(U& that, float& updatedValue )        //10
     {
-        if (that == nullptr)
-        {
-            std::cout << "that is nullptr" << std::endl;
-            return 0.f;
-        }
-
-        if (updatedValue == nullptr)
-        {
-            std::cout << "updatedValue is nullptr" << std::endl;
-            return 0.f;
-        }
- 
-
-        std::cout << "U's nameOne value: " << that->nameOne << std::endl;
-        that->nameOne = *updatedValue;
+        std::cout << "U's nameOne value: " << that.nameOne << std::endl;
+        that.nameOne = updatedValue;
         
-        std::cout << "U's nameOne updated value: " << that->nameOne << std::endl;
+        std::cout << "U's nameOne updated value: " << that.nameOne << std::endl;
 
 
-        while( std::abs(that->nameTwo - that->nameOne) > 0.001f )
+        while( std::abs(that.nameTwo - that.nameOne) > 0.001f )
         {
             /*
+              
             write something that makes the distance between that->nameTwo and        that->nameOne get smaller
             */
-            auto tempTwo = std::abs(that->nameTwo - that->nameOne);
+            auto tempTwo = std::abs(that.nameTwo - that.nameOne);
         
-            that->nameTwo += 0.1f;
+            that.nameTwo += 0.1f;
 
             std::cout << "shrinking distance\n" << tempTwo << std::endl;
         }
-        std::cout << "U's nameTwo updated value: " << that->nameTwo << std::endl;
+        std::cout << "U's nameTwo updated value: " << that.nameTwo << std::endl;
         
-        return that->nameTwo * that->nameOne;
+        return that.nameTwo * that.nameOne;
     }
 };
 
@@ -134,13 +122,15 @@ int main()
     
     U nameThree;
     float updatedValue = 5.f;
-    std::cout << "[static func] nameThree's multiplied values: " << StructNameTwo::staticFunctionA(&nameThree, &updatedValue ) << std::endl;                  //11
+    std::cout << "[static func] nameThree's multiplied values: " << StructNameTwo::staticFunctionA(nameThree, updatedValue ) << std::endl;                  //11
     
     U nameFour;
-    std::cout << "[member func] nameFour's multiplied values: " << nameFour.staticFunctionReplicate( &updatedValue ) << std::endl;
+    std::cout << "[member func] nameFour's multiplied values: " << nameFour.staticFunctionReplicate( updatedValue ) << std::endl;
 
-    StructNameTwo::staticFunctionA(nullptr, &updatedValue );
-    StructNameTwo::staticFunctionA(&nameThree, nullptr );
+        
+
+    // StructNameTwo::staticFunctionA(nullptr, &updatedValue );
+    // StructNameTwo::staticFunctionA(&nameThree, nullptr );
 
     std::cout << "this is the end" << std::endl;
 }
